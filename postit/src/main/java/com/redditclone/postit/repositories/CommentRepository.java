b69653811076c2448cb15f8ed155b7e59d9f74af
maheenmatin <maheenmatin@gmail.com>
2024-08-29 18:52:55 +0100

    Add post functionality
    Also add "PrettyTime" dependency

--- postit/src/main/java/com/redditclone/postit/repositories/CommentRepository.java
+++ postit/src/main/java/com/redditclone/postit/repositories/CommentRepository.java
@@ -1,7 +1,10 @@
 package com.redditclone.postit.repositories;
 
 import com.redditclone.postit.models.Comment;
+import com.redditclone.postit.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
 }
--- postit/src/main/java/com/redditclone/postit/repositories/SubredditRepository.java
+++ postit/src/main/java/com/redditclone/postit/repositories/SubredditRepository.java
@@ -2,6 +2,8 @@ package com.redditclone.postit.repositories;
 
 import com.redditclone.postit.models.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+    Optional<Subreddit> findByName(String subredditName);
 }
