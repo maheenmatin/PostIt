cb6192b65af0d9b51bc3e32980204b83f03cff93
maheenmatin <maheenmatin@gmail.com>
2024-09-08 18:35:51 +0100

    Add refresh token functionality

--- postit/src/main/java/com/redditclone/postit/controllers/AuthController.java
+++ postit/src/main/java/com/redditclone/postit/controllers/AuthController.java
@@ -2,33 +2,48 @@ package com.redditclone.postit.controllers;
 
 import com.redditclone.postit.dto.AuthenticationResponse;
 import com.redditclone.postit.dto.LoginRequest;
+import com.redditclone.postit.dto.RefreshTokenRequest;
 import com.redditclone.postit.dto.RegisterRequest;
 import com.redditclone.postit.services.AuthService;
+import com.redditclone.postit.services.RefreshTokenService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
-        return new ResponseEntity<>("User registration successful", HttpStatus.OK);
+        return new ResponseEntity<>("User registration successful", OK);
     }
 
     @GetMapping("accountVerification/{token}")
     public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
-        return new ResponseEntity<>("Account activated successfully", HttpStatus.OK);
+        return new ResponseEntity<>("Account activated successfully", OK);
     }
 
     @PostMapping("/login")
     public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
         return authService.login(loginRequest);
+    }
+
+    @PostMapping("/refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(OK).body("Refresh token deleted successfully");
     }
 }
--- postit/src/main/java/com/redditclone/postit/services/AuthService.java
+++ postit/src/main/java/com/redditclone/postit/services/AuthService.java
@@ -2,6 +2,7 @@ package com.redditclone.postit.services;
 
 import com.redditclone.postit.dto.AuthenticationResponse;
 import com.redditclone.postit.dto.LoginRequest;
+import com.redditclone.postit.dto.RefreshTokenRequest;
 import com.redditclone.postit.dto.RegisterRequest;
 import com.redditclone.postit.exceptions.PostItException.PostItException;
 import com.redditclone.postit.models.NotificationEmail;
@@ -34,6 +35,7 @@ public class AuthService {
     private final MailService mailService;
     private final AuthenticationManager authenticationManager;
     private final JwtProvider jwtProvider;
+    private final RefreshTokenService refreshTokenService;
 
     @Transactional
     public void signup(RegisterRequest registerRequest) {
@@ -90,9 +92,26 @@ public class AuthService {
                 loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authenticate);
-
         String token = jwtProvider.generateToken(authenticate);
-        return new AuthenticationResponse(token, loginRequest.getUsername());
+
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .refreshToken(refreshTokenService.generateRefreshToken().getToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(loginRequest.getUsername())
+                .build();
+    }
+
+    public AuthenticationResponse refreshToken(RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.validateRefreshToken(refreshTokenRequest.getRefreshToken());
+        String token = jwtProvider.generateTokenWithUserName(refreshTokenRequest.getUsername());
+
+        return AuthenticationResponse.builder()
+                .authenticationToken(token)
+                .refreshToken(refreshTokenRequest.getRefreshToken())
+                .expiresAt(Instant.now().plusMillis(jwtProvider.getJwtExpirationInMillis()))
+                .username(refreshTokenRequest.getUsername())
+                .build();
     }
 
     @Transactional(readOnly = true)
