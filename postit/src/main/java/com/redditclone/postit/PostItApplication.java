2041bb4d9b0f6a8167b250cf6c4d0e124414c507
maheenmatin <maheenmatin@gmail.com>
2024-09-18 23:36:28 +0100

    Generate API documentation

--- postit/src/main/java/com/redditclone/postit/PostItApplication.java
+++ postit/src/main/java/com/redditclone/postit/PostItApplication.java
@@ -1,13 +1,18 @@
 package com.redditclone.postit;
 
+import com.redditclone.postit.configuration.OpenAPIConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(OpenAPIConfig.class)
 public class PostItApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PostItApplication.class, args);
 	}
 }
+
+//documentation link -> http://localhost:8080/swagger-ui/index.html
