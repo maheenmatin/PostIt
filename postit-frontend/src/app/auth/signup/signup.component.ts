477205a2f0e0a4f72bc1fede4b3b2f9fdad4b4a9
maheenmatin <maheenmatin@gmail.com>
2024-09-19 01:51:20 +0100

    Add frontend signup feature

--- postit-frontend/src/app/auth/signup/signup.component.ts
+++ postit-frontend/src/app/auth/signup/signup.component.ts
@@ -1,25 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { SignupRequestPayload } from "./signup-request.payload";
+import { AuthService } from "../shared/auth.service";
 
 @Component({
-  selector: 'app-signup',
+  selector: "app-signup",
   standalone: true,
   imports: [RouterModule, ReactiveFormsModule, CommonModule],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  templateUrl: "./signup.component.html",
+  styleUrl: "./signup.component.css",
 })
 export class SignupComponent {
+  signupRequestPayload: SignupRequestPayload;
   signupForm!: FormGroup;
 
-  constructor() {}
+  constructor(private authService : AuthService) {
+    this.signupRequestPayload = {
+      username: "",
+      email: "",
+      password: "",
+    };
+  }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      username: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', Validators.required),
+      username: new FormControl("", Validators.required),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", Validators.required),
     });
+  }
+
+  signup() {
+    this.signupRequestPayload.email = this.signupForm.get("email")!.value;
+    this.signupRequestPayload.username = this.signupForm.get("username")!.value;
+    this.signupRequestPayload.password = this.signupForm.get("password")!.value;
+
+    this.authService.signup(this.signupRequestPayload)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
