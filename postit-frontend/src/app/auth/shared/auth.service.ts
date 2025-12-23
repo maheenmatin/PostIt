3c3e1aed892568d8662510f57a8871ef2af4dc08
maheenmatin <maheenmatin@gmail.com>
2025-12-23 12:52:45 +0000

    Fix registration error bug

--- postit-frontend/src/app/auth/shared/auth.service.ts
+++ postit-frontend/src/app/auth/shared/auth.service.ts
@@ -18,10 +18,8 @@ export class AuthService {
   constructor(private httpClient: HttpClient, private localStorage: LocalStorageService, private router: Router) {}
 
   // Register a new account.
-  signup(signupRequestPayload: SignupRequestPayload): Observable<string> {
-    return this.httpClient.post(`${API_ENDPOINTS.auth}/signup`, signupRequestPayload, {
-      responseType: "text",
-    });
+  signup(signupRequestPayload: SignupRequestPayload): Observable<void> {
+    return this.httpClient.post<void>(`${API_ENDPOINTS.auth}/signup`, signupRequestPayload);
   }
 
   // Authenticate and cache JWT/refresh tokens locally.
