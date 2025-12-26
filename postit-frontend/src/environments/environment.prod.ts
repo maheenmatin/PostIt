5d225d65857c4cfd648d73e1bb6e2279d0902663
maheenmatin <maheenmatin@gmail.com>
2025-12-26 17:08:41 +0000

    Fix prod environment bug

--- postit-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'https://postit-x5e6.onrender.com',
-};
--- postit-frontend/src/environments/environment.ts
+++ postit-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8080',
+  production: true,
+  apiBaseUrl: 'https://postit-x5e6.onrender.com',
 };
